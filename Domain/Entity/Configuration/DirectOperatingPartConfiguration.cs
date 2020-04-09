a00dd3e81f62f02c3356c3a4ee0a3a413b7e06c3
safaa-shade <sofi-shade@hotmail.com>
2020-04-10 00:57:37 +0300

    directOpPart

---
+++ Domain/Entity/Configuration/DirectOperatingPartConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entity.ProjectEntity.Clinic.Equipment.DiagnosticEquipment.Part;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.Configuration
+{
+    public class DirectOperatingPartConfiguration : EntityTypeConfiguration<DirectOperatingPart>
+    {
+        public DirectOperatingPartConfiguration()
+        {
+            ToTable("DirectOperatingPart");
+            HasKey(x => x.Id)
+                .Property(x => x.Id)
+                .HasColumnName("Id")
+                .HasColumnType("int")
+                .IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            
+                 Property(x => x.CodeInStuck)
+                .HasColumnName("CodeInStuck")
+                .HasColumnType("nvarchar")
+                .IsRequired();
+
+            Property(x => x.Date)
+                .HasColumnName("Date")
+                .HasColumnType("nvarchar")
+                .IsRequired();
+
+            Property(x => x.Note)
+                .HasColumnName("Note")
+                .HasColumnType("nvarchar");
+
+            HasRequired(x => x.DirectOperatingPartName).WithMany(x => x.DirectOperatingPart);
+
+            // its done
+
+        }
+    }
+}
